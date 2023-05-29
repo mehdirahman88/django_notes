97785ef175a29538446b63365a08b4a08a872305
mehdirahman88 <hide.m88@gmail.com>
2023-05-29 19:28:14 +0700

    Fix test structure

--- note/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from .forms_tests import NoteFormsTestCase
-from .model_tests import NoteModelTestCase
-from .views_tests import NoteViewsCRUDTestCase, NoteViewsAuthTestCase, IndexViewSearchTestCase
-
